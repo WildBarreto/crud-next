ccafc6a8d1cb651e6e973c42a19a437324abd4b1
WildBarreto <wildsbsrreto@gmail.com>
2023-05-01 20:42:22 -0300

    Integração do cadastro com Firebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,77 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useEffect, useState } from "react";
+import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Clients";
+import ClienteRepositorio from "../core/ClienteRepositorio";
+import ColecaoCliente from "../backend/db/ColecaoCliente";
 
 export default function Home() {
+  const repo: ClienteRepositorio = new ColecaoCliente();
+
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio());
+  const [clientes, setClientes] = useState<Cliente[]>([]);
+  const [visivel, setVisivel] = useState<"tabela" | "form">("tabela");
+
+  useEffect(obterTodos, []);
+
+  function obterTodos() {
+    repo.obterTodos().then(clientes => {
+      setClientes(clientes)
+      setVisivel('tabela')
+    });
+  }
+
+  function clienteSelecionado(cliente: Cliente) {
+    setCliente(cliente);
+    setVisivel("form");
+  }
+
+  async function clienteExcluido(cliente: Cliente) {
+   await repo.excluir(cliente)
+   obterTodos()
+  }
+
+  function novoCliente() {
+    setCliente(Cliente.vazio());
+    setVisivel("form");
+  }
+
+  async function salvarCliente(cliente: Cliente) {
+    await repo.salvar(cliente);
+    obterTodos()
+    
+  }
+
+  
+
   return (
-    <div className={`flex h-screen justify-center items-center bg-gradient-to-r from-purple-400 to-blue-300`}>
-      <span className="text-4xl">Texto</span>
+    <div
+      className={`flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white`}
+    >
+      <Layout titulo="Cadastro Simples">
+        {visivel === "tabela" ? (
+          <>
+            <div className="flex justify-end">
+              <Botao className="mb-4" onClick={novoCliente}>
+                Novo Cliente
+              </Botao>
+            </div>
+            <Tabela
+              clientes={clientes}
+              clienteSelecionado={clienteSelecionado}
+              clienteExcluido={clienteExcluido}
+            ></Tabela>
+          </>
+        ) : (
+          <Formulario
+            cliente={cliente}
+            clienteMudou={salvarCliente}
+            cancelado={() => setVisivel("tabela")}
+          />
+        )}
+      </Layout>
     </div>
   );
 }
